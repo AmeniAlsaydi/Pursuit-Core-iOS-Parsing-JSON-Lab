1dc970bea40cf2fc9229f2a5884d0aede89e47b4
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-27 12:33:52 -0500

    random user data poplulates table view

---
+++ Parsing_JSON_Lab/Parsing_JSON_Lab/RandomUser.swift
@@ -0,0 +1,66 @@
+//
+//  RandomUser.swift
+//  Parsing_JSON_Lab
+//
+//  Created by Amy Alsaydi on 11/27/19.
+//  Copyright © 2019 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
+
+struct RandomUserData: Codable {
+    let results: [User]
+}
+
+struct User: Codable {
+    let name: Name
+    let email: String
+    let dob: DateInfo
+    let phone: String
+    let location: AddressInfo
+    
+}
+
+struct Name: Codable  {
+    let first: String
+    let last: String
+}
+
+struct DateInfo: Codable {
+    let date: String
+}
+
+struct AddressInfo: Codable {
+    let street: StreetInfo
+    let city: String
+    let state: String
+    let country: String
+    // let postcode: Int
+}
+
+struct StreetInfo: Codable {
+    let number: Int
+    let name: String
+}
+
+
+extension RandomUserData {
+    static func getRandomUsers() -> [User] {
+        var randomUsers = [User]()
+        
+        guard let fileURL = Bundle.main.url(forResource: "randomUserData", withExtension: "json") else {
+            fatalError("could not locate json file")
+        }
+        
+        do {
+            let data = try Data(contentsOf: fileURL)
+            
+            let userData = try JSONDecoder().decode(RandomUserData.self, from: data)
+            randomUsers = userData.results
+        } catch {
+            fatalError("failed to load from contents \(error)")
+        }
+        return randomUsers
+    }
+    
+}
---
+++ Parsing_JSON_Lab/Parsing_JSON_Lab/UserController.swift
@@ -0,0 +1,51 @@
+//
+//  UserController.swift
+//  Parsing_JSON_Lab
+//
+//  Created by Amy Alsaydi on 11/27/19.
+//  Copyright © 2019 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class UserController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var users = [User]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        loadData() 
+
+    }
+    
+    func loadData() {
+        users = RandomUserData.getRandomUsers()
+    }
+
+
+}
+extension UserController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.name.first
+        cell.detailTextLabel?.text = user.email
+        
+        return cell
+    }
+    
+    
+}
